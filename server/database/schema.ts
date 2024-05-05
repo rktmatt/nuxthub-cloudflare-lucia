7491d2e376133e5caccd0e978dc8c07b2ec12fb5
Matthieu Roussel <matthieu0roussel@gmail.com>
2024-05-05 22:37:32 +0200

    usinq d1 adapter

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,38 +1,45 @@
-import { sqliteTable, text, integer, blob } from "drizzle-orm/sqlite-core";
-import { sql } from "drizzle-orm";
+import {
+  sqliteTable,
+  text,
+  integer,
+  blob,
+} from 'drizzle-orm/sqlite-core'
+import { sql } from 'drizzle-orm'
 
-export type UserId = string & { __brand: "userId" };
+export type UserId = string & { __brand: 'userId' }
 
 /**
  * Session table
  */
-export const session = sqliteTable("session", {
-  id: text("id").notNull().primaryKey(),
-  userId: text("user_id")
+export const session = sqliteTable('session', {
+  id: text('id').notNull().primaryKey(),
+  userId: text('user_id')
     .notNull()
     .references(() => user.id),
-  expiresAt: integer("expiresAt").notNull(),
-});
+  expiresAt: integer('expiresAt').notNull(),
+})
 
 /**
  * Challenge table
  */
-export const challenge = sqliteTable("challenge", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  challenge: blob("challenge").notNull(),
+export const challenge = sqliteTable('challenge', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  challenge: blob('challenge').notNull(),
   //userId: text("userId")
   //  .notNull()
   //  .references(() => user.id),
-});
+})
 
 /**
- * This represent users using the app
+ * User table
  */
-export const user = sqliteTable("user", {
-  id: text("id").$type<UserId>().primaryKey(),
-  lastName: text("lastName"),
-  firstName: text("firstName"),
-  email: text("email").notNull().unique(),
-  createdAt: text("createdAt").default(sql`(CURRENT_TIMESTAMP)`),
-  role: text("role", { enum: ["user", "admin"] }).default("user"),
-});
+export const user = sqliteTable('user', {
+  id: text('id').$type<UserId>().primaryKey(),
+  lastName: text('lastName'),
+  firstName: text('firstName'),
+  email: text('email').notNull().unique(),
+  createdAt: text('createdAt').default(sql`(CURRENT_TIMESTAMP)`),
+  role: text('role', { enum: ['user', 'admin'] }).default(
+    'user'
+  ),
+})
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,30 +1,14 @@
-import { drizzle } from "drizzle-orm/d1";
-export { sql, eq, and, or } from "drizzle-orm";
-import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
-import { Lucia } from "lucia";
+import { drizzle } from 'drizzle-orm/d1'
+export { sql, eq, and, or } from 'drizzle-orm'
 
-import * as schema from "../database/schema";
+import * as schema from '../database/schema'
 
-export const tables = schema;
+export const tables = schema
 
 export function useDrizzle() {
-  return drizzle(hubDatabase(), { schema });
+  return drizzle(hubDatabase(), { schema })
 }
 
-const adapter = new DrizzleSQLiteAdapter(
-  useDrizzle(),
-  tables.session,
-  tables.user,
-);
-
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: !process.dev,
-    },
-  },
-});
-
-export type User = typeof schema.user.$inferSelect;
-export type Challenge = typeof schema.challenge.$inferSelect;
-export type Session = typeof schema.session.$inferSelect;
+export type User = typeof schema.user.$inferSelect
+export type Challenge = typeof schema.challenge.$inferSelect
+export type Session = typeof schema.session.$inferSelect
